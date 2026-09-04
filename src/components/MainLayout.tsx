86fbbdbad5c1c7562b2119013035c795a2855d85
Tomer <tomer10.katz@gmail.com>
2026-09-04 11:19:33 +0300

    add main layout
    in the layout there is the appbar and sidebar

---
+++ src/components/MainLayout.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import { Appbar } from "./Appbar";
+import { Sidebar } from "./Sidebar";
+
+export default function MainLayout() {
+    const [isDrawerOpen, setDrawerOpen] = useState<boolean>(false);
+
+    return (
+        <div className="min-h-screen bg-gray-950 text-gray-100 flex flex-col font-sans">
+            <Appbar onOpenDrawer={() => setDrawerOpen(true)} />
+            <Sidebar isOpen={isDrawerOpen} onClose={() => setDrawerOpen(false)} />
+            <main className="flex-1 p-6 items-center justify-center w-full">
+                <Outlet />
+            </main>
+        </div>
+    );
+}
