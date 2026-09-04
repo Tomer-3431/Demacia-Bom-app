ad4a683ceb5476c6b8f69645379cbc7e396d794b
Tomer <tomer10.katz@gmail.com>
2026-09-04 11:46:58 +0300

    change the jsx to tsx files

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import './css/App.css';
+import Home from './pages/HomePage';
+import MainLayout from './components/MainLayout';
+import { OnshapePage } from './pages/OnshapePage';
+
+export default function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/onshape" element={<OnshapePage />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
+}
