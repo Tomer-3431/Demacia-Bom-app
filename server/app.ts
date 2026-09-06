9fbda840a3025a6237f3d3c81caef502d4647989
Tomer <tomer10.katz@gmail.com>
2026-09-06 19:17:11 +0300

    add auth
    must have secret key and allowed origin to use server

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,28 @@
 import express, { Request, Response, ErrorRequestHandler, Express, NextFunction } from "express";
 import cors from "cors";
 import apiRoutes from './routes';
+import { checkAuth } from "./controllers/healthController";
 
 const app: Express = express();
 
-app.use(cors());
+export const allowedOrigins = (process.env.ALLOWED_ORIGINS || '')
+    .split(', ')
+    .map((origin) => origin.trim())
+    .filter(Boolean);
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("CORS policy violation: Access denied"));
+        }
+    },
+    credentials: true
+}));
 app.use(express.json());
 
-app.use('/api', apiRoutes)
+app.use('/api', checkAuth, apiRoutes)
 
 app.use((req, res) => {
     res.status(404).json({ message: `Route ${req.method} ${req.originalUrl} not found`});
--- server/controllers/healthController.ts
+++ server/controllers/healthController.ts
@@ -1,16 +1,30 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { isConnected } from "../db/connection";
+import { allowedOrigins } from "../app";
+import dotenv from 'dotenv';
 
-export function checkAuth(req: Request, res: Response) {
-  // TODO: add authorization
-  const authorized = true;
+dotenv.config();
 
-  if (!authorized)
-    return res.status(401).json({ authorized: false, message: "Unauthorized" });
-  return res.status(200).json({ authorized: true, message: "OK" });
+const CLIENT_SECRET_KEY = process.env.CLIENT_SECRET;
+
+export function checkAuth(req: Request, res: Response, next: NextFunction) {
+  const origin = req.headers.origin;
+
+  if (origin && !allowedOrigins.includes(origin)) {
+    return res.status(403).json({ messeage: 'Origin not allowed' });
+  }
+
+  const clientSecret = req.headers['x-client-secret'];
+
+  if (clientSecret !== CLIENT_SECRET_KEY) {
+    return res.status(401).json({ message: 'Invalid or missing client secrets' });
+  }
+
+
+  return next();
 }
 
 export function checkDBConnection(req: Request, res: Response) {
-    const connected = isConnected();
-    return res.status(connected ? 200 : 503).json({ connected });
+  const connected = isConnected();
+  return res.status(connected ? 200 : 503).json({ connected });
 }
