3b8304307640fc507f1d5dc86082fd21c68a160a
Tomer <tomer10.katz@gmail.com>
2026-09-06 18:30:51 +0300

    add all routes to the app

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response, ErrorRequestHandler, Express, NextFunction } from "express";
 import cors from "cors";
+import apiRoutes from './routes';
 
 const app: Express = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api', apiRoutes)
 
 app.use((req, res) => {
     res.status(404).json({ message: `Route ${req.method} ${req.originalUrl} not found`});
