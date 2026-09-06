25b4ec99bd684e2ae398b58240a7e6df10d1466f
Tomer <tomer10.katz@gmail.com>
2026-09-06 13:25:38 +0300

    split server to app

---
+++ server/app.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response, ErrorRequestHandler, Express, NextFunction } from "express";
+import cors from "cors";
+
+const app: Express = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res) => {
+    res.status(404).json({ message: `Route ${req.method} ${req.originalUrl} not found`});
+});
+
+interface HttpError extends Error {
+    status?: number;
+}
+
+const errorHandler: ErrorRequestHandler = (
+    err: HttpError,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    console.error(err.stack);
+    res.status(err.status || 500).json({ message: err.message || "Internal server error" });
+}
+app.use(errorHandler);
+
+export default app;
--- server/server.ts
+++ server/server.ts
@@ -1,26 +1,14 @@
-import express, { Express } from "express";
-import cors from "cors";
 import dotenv from 'dotenv';
 import dns from 'node:dns';
-import { MongoClient } from 'mongodb';
+import app from './app';
 
 dotenv.config();
-
 dns.setDefaultResultOrder("ipv4first");
 
 const PORT = process.env.PORT || 5050;
-const mongoUri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017";
-const client = new MongoClient(mongoUri);
-const app: Express = express();
-
-app.use(cors());
-app.use(express.json());
 
 async function startServer(): Promise<void> {
   try {
-    await client.connect();
-    console.log("Succeessfully connected to MongoDB Atlas");
-
     app.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`);
     });
