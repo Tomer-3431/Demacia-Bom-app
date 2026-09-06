7a396ab5e98f753e653edd036f1e5666a1ea75a9
Tomer <tomer10.katz@gmail.com>
2026-09-06 13:28:26 +0300

    create work order controller

---
+++ server/controllers/workOrderController.ts
@@ -0,0 +1,96 @@
+import { NextFunction, Request, Response } from "express";
+import WorkOrder from "../models/WorkOrder";
+
+interface SubPartBody {
+    partID: string;
+    quantityTotal?: number;
+    quantityMade?: number;
+    statusCode?: number;
+    productionGCOwner?: string;
+    productionMakingOwner?: string;
+    lastUpdate?: Date;
+    firstAdded?: Date
+}
+
+interface WorkOrderBody {
+    name?: string;
+    bomID?: string;
+    workOrderOwner?: string;
+    description?: string;
+
+    parts?: SubPartBody[];
+
+    comments?: string;
+    workOrderCreated?: Date;
+}
+
+export async function getAllWorkOrders(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    try {
+        const workOrders = await WorkOrder.find().sort({ _id: 1 });
+        return res.status(200).json(workOrders);
+    } catch (err) {
+        return next(err);
+    }
+}
+
+export async function getWorkOrderByID(
+    req: Request<{ id: string }>,
+    res: Response,
+    next: NextFunction
+) {
+    try {
+        const id = req.params.id;
+        const workOrder = WorkOrder.findOne({ _id: id });
+        if (!workOrder) 
+            return res.status(404).json({ message: `Work Order ${id} not found` });
+        return res.status(200).json(workOrder);
+    } catch (err) {
+        return next(err);
+    }
+}
+
+export async function upsertWorkOrderByID(
+    req: Request<{ id: string }, unknown, WorkOrderBody>,
+    res: Response,
+    next: NextFunction
+) {
+    try {
+        const id = req.params.id;
+        const existing = await WorkOrder.findOne({ _id: id });
+        const workOrder = await WorkOrder.findByIdAndUpdate(
+            {_id: id},
+            { ...req.body, _id: id},
+            {
+                new: true,
+                upsert: true,
+                runValidators: true,
+                setDefaultsOnInsert: true
+            }
+        );
+
+        return res.status(existing ? 200 : 201).json(workOrder);
+    } catch (err) {
+        return next(err);
+    }
+}
+
+export async function deleteWorkOrderByID(
+    req: Request<{ id: string }>,
+    res: Response,
+    next: NextFunction
+) {
+    try {
+        const id = req.params.id;
+        const deleted = await WorkOrder.findOneAndDelete({ _id: id });
+
+        if (!deleted)
+            return res.status(404).json({ message: `Work order ${id} not found`});
+        return res.status(204).json();
+    } catch (err) {
+        return next(err);
+    }
+}
