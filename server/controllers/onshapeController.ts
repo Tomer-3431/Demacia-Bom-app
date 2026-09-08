2a19a0b746ae65225af3e23bdde09149ef418e81
Tomer <tomer10.katz@gmail.com>
2026-09-08 18:58:18 +0300

    add controllers to all of the new functions from service

--- server/controllers/onshapeController.ts
+++ server/controllers/onshapeController.ts
@@ -87,5 +87,153 @@ export async function getBom(req: Request<OnshapeBomParams>, res: Response) {
     return handleOnshapeError(res, err, "Failed to fetch bom from Onshape");
   }
 }
+
+export async function getPartThumbnail(
+  req: Request<OnshapePartParams & { size?: string }>,
+  res: Response,
+) {
+  try {
+    const thumbnail = await onshapeService.getPartThumbnail(
+      req.params,
+      req.params.size,
+    );
+    if (!thumbnail)
+      return res
+        .status(404)
+        .json({ message: `onshape part was not found ${req.params}` });
+    res.setHeader("Content-Type", "image/png");
+    return res.status(200).json(thumbnail);
+  } catch (err) {
+    return handleOnshapeError(res, err, "Failed to fetch thumbnail for part");
+  }
+}
+
+export async function setPartThumbnail(
+  req: Request<OnshapePartParams, unknown, Buffer>,
+  res: Response,
+) {
+  try {
+    const buffer = req.body;
+    let safeBuffer: Buffer;
+
+    if (Buffer.isBuffer(buffer)) {
+      safeBuffer = buffer;
+    } else if (
+      buffer &&
+      typeof buffer === "object" &&
+      "data" in buffer &&
+      Array.isArray((buffer as any).data)
+    ) {
+      safeBuffer = Buffer.from((buffer as any).data);
+    } else {
+      return res.status(400).json({
+        message: `Invalid buffer provided for file upload: expected Buffer, got ${typeof buffer}`,
+      });
+    }
+
+    await onshapeService.setPartThumbnail(req.params, safeBuffer);
+
+    return res.status(204);
+  } catch (err) {
+    return handleOnshapeError(res, err, "Failed to set thumbnail for part");
+  }
+}
+
+export async function getElementThumbnail(
+  req: Request<OnshapeBomParams & { size?: string }>,
+  res: Response,
+) {
+  try {
+    const thumbnail = await onshapeService.getElementThumbnail(
+      req.params,
+      req.params.size,
+    );
+    if (!thumbnail)
+      return res
+        .status(404)
+        .json({ message: `onshape element was not found ${req.params}` });
+
+    res.setHeader("Content-Type", "image/png");
+    return res.status(200).json(thumbnail);
+  } catch (err) {
+    return handleOnshapeError(
+      res,
+      err,
+      "Failed to fetch thumbnail for element",
+    );
+  }
+}
+
+export async function setElementThumbnail(
+  req: Request<OnshapeBomParams, unknown, Buffer>,
+  res: Response,
+) {
+  try {
+    const buffer = req.body;
+    let safeBuffer: Buffer;
+
+    if (Buffer.isBuffer(buffer)) {
+      safeBuffer = buffer;
+    } else if (
+      buffer &&
+      typeof buffer === "object" &&
+      "data" in Buffer &&
+      Array.isArray((buffer as any).data)
+    ) {
+      safeBuffer = Buffer.from((buffer as any).data);
+    } else {
+      return res.status(400).json({ message: `Invalid buffer provided for file upload: expected Buffer, got ${typeof buffer}`});
+    }
+
+    await onshapeService.setElementThumbnail(req.params, safeBuffer);
+    
+    return res.status(204);
+  } catch (err) {
+    return handleOnshapeError(res, err, "Failed to set thumbnail for element");
+  }
+}
+
+export async function exportSTL(
+  req: Request<OnshapePartParams>,
+  res: Response
+) {
+  try {
+    const stl = await onshapeService.exportPartToStl(req.params);
+    if (!stl) res.status(404).json({ message: `part not found ${req.params}`});
+
+    res.setHeader("Content-Type", "model/stl");
+    return res.status(200).json(stl);
+  } catch (err) {
+    return handleOnshapeError(res, err, 'Failed to export stl');
+  }
+}
+
+export async function exportParasolid(
+  req: Request<OnshapePartParams>,
+  res: Response
+) {
+  try {
+    const parasolid = await onshapeService.exportPartToParasolid(req.params);
+    if (!parasolid) res.status(404).json({ message: `part not found ${req.params}`});
+
+    res.setHeader("Content-Type", "application/x-parasolid");
+    return res.status(200).json(parasolid);
+  } catch (err) {
+    return handleOnshapeError(res, err, 'Failed to export parasolid');
+  }
+}
+
+export async function exportSolidworks(
+  req: Request<OnshapePartParams>,
+  res: Response
+) {
+  try {
+    const solidworks = await onshapeService.exportPartToSolidworks(req.params);
+    if (!solidworks) res.status(404).json({ message: `part not found ${req.params}`});
+
+    res.setHeader("Content-Type", "application/sldprt");
+    return res.status(200).json(solidworks);
+  } catch (err) {
+    return handleOnshapeError(res, err, 'Failed to export solidworks');
   }
 }
