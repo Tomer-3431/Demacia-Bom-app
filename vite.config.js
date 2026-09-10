721620cb18c6a75ac70970db52ca567ceb94fd26
Edward <eyal.bar.games@gmail.com>
2026-09-10 15:01:34 +0300

    apparently vite fucks me in the asshole

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import mkcert from 'vite-plugin-mkcert';
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
-  server: {
-    https: true,
-    port: 5173
-  }
-});
+  plugins: [react(), tailwindcss()],
+  base: '/BomApp/'
+})
